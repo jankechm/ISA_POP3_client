4aee51482e7cd0468bc3b29ebc7b8a0f7a1f4e5c
Marek Jankech <marek.jankech@outlook.com>
2017-11-15 23:21:21 +0100

    Initial commit

---
+++ popcl.cpp
@@ -0,0 +1,82 @@
+/**
+ * File:        popcl.cpp
+ * Author:      Marek Jankech
+ * Date:        20.11.2017
+ * Project:     Client POP3 with TLS support
+ * Description: The program allows reading mails through POP3 protocol
+                with pop3s and POP3 STARTTLS extensions. Program supports
+                authentication with USER/PASS commands only.
+ */
+
+#include <cstdlib>
+#include <cstdio>
+#include <cstdbool>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+
+using namespace std;
+
+/**
+ *Prototypes of functions
+ */
+void errTerminate(string msg);
+
+/**
+ *Main
+ */
+int main(int argc, char **argv)
+{
+    int c;
+    bool pFlag, TFlag, SFlag, cFlag, CFlag, dFlag, nFlag, aFlag, oFlag;
+    string server, port, certFile, certAddr, authFile, outDir;
+
+    cout << "Hello world!" << endl;
+    while ((c = getopt(argc, argv, ":p:TSc:C:dna:o:")) != -1) {
+      switch (c) {
+        case 'p':
+          pFlag = true;
+          port = optarg;
+          break;
+        case 'T':
+          TFlag = true;
+          break;
+        case 'S':
+          SFlag = true;
+          break;
+        case 'c':
+          cFlag = true;
+          certFile = optarg;
+          break;
+        case 'C':
+          CFlag = true;
+          certAddr = optarg;
+          break;
+        case 'd':
+          dFlag = true;
+          break;
+        case 'n':
+          nFlag = true;
+          break;
+        case 'a':
+          aFlag = true;
+          authFile = optarg;
+          break;
+        case 'o':
+          oFlag = true;
+          outDir = optarg;
+          break;
+        case ':':
+          break;
+      }
+    }
+    return 0;
+}
+
+/**
+ * Function for terminating the program with an error message
+ */
+void errTerminate(string msg) {
+  cerr << "Error: " << msg << "\n";
+  exit(EXIT_FAILURE);
+}
